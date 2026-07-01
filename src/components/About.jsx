689b31c7135418544d093e0aae80b4d86b1bafba
rahil-shaikh <rahil@griphhy.com>
2026-07-01 16:25:00 +0530

    Updated site info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ export default function About() {
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.6 }}
-            className="grid grid-cols-2 gap-3 auto-rows-min"
+            className="grid grid-cols-2 gap-3 auto-rows-fr"
           >
             {siteInfo.stats.map((stat) => (
               <div
